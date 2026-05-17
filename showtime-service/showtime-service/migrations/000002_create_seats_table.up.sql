0128fb409b5e63fb30cbc5fd832b20e148d4862d
Ленара Сымбаткызы <symbatkyzylenara@gmail.com>
2026-05-17 17:56:43 +0500

    chore(showtime): remove binaries and duplicate folder, add .gitignore

--- showtime-service/showtime-service/migrations/000002_create_seats_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE seats (
-    id UUID PRIMARY KEY,
-    showtime_id UUID NOT NULL REFERENCES showtimes(id),
-    row VARCHAR(5) NOT NULL,
-    number INT NOT NULL,
-    is_available BOOLEAN DEFAULT true
-);
